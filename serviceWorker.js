e07bf254acf567899427d2de406eeedf5365789a
lonlazer <leon@lazar-rudolf.de>
2021-03-02 17:09:21 +0100

    First commit

---
+++ serviceWorker.js
@@ -0,0 +1,48 @@
+// Perform install steps
+let cacheName = 'TFHoloPoC_Cache';
+let contentToCache = [
+    'https://aframe.io/releases/1.2.0/aframe.min.js',
+    'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.7.4/dist/tf.min.js',
+    'https://unpkg.com/@tensorflow-models/mobilenet',
+    'registerServiceWorker.js',
+    'serviceWorker.js',
+    'tfHoloPoc.js',
+    'index.html'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(contentToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+      caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+          if(key !== cacheName) {
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+  });
