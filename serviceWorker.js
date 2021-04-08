d542d8d40b619d0e9671ad30cbbf8b22fc703f47
lonlazer <leon@lazar-rudolf.de>
2021-04-09 00:19:11 +0200

    Change serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -27,28 +27,26 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) { return r; }
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
 });
 
 self.addEventListener('activate', (e) => {
     e.waitUntil(
-      caches.keys().then((keyList) => {
+        caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
-          if(key !== cacheName) {
-            return caches.delete(key);
-          }
-        }));
-      })
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
     );
-  });
+});
