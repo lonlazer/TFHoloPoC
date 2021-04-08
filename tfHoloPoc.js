31fbb7a62219415be40f3d4f0d9c1f98d62ed695
lonlazer <leon@lazar-rudolf.de>
2021-04-08 21:47:31 +0200

    Model change

--- tfHoloPoc.js
+++ tfHoloPoc.js
@@ -1,40 +1,22 @@
 var net;
-var webcamElement;
 var webcam;
 var videoElement;
 
-async function createVideoElement() {
-    webcamElement = document.createElement('video');
-    webcamElement.setAttribute("autoplay", "");
-    webcamElement.setAttribute("playsinline", "");
-    webcamElement.setAttribute("width", "224");
-    webcamElement.setAttribute("height", "224");
-}
-
-async function setupWebcam() {
-    return new Promise((resolve, reject) => {
-        const navigatorAny = navigator;
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-            navigatorAny.msGetUserMedia;
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia({ video: true },
-                stream => {
-                    webcamElement.srcObject = stream;
-                    webcamElement.addEventListener('loadeddata', () => resolve(), false);
-                },
-                error => reject());
-        } else {
-            reject();
-        }
-    });
-}
+ async function captureImage() {
+    const img = await webcam.capture();
+    const processedImg =
+        tf.tidy(() => img.expandDims(0).toFloat().div(127).sub(1));
+    img.dispose();
+    return img;
+  }
 
 async function run() {
     while (true) {
 
+        let image = await captureImage();
+
         var start = new Date().getTime();
-        const result = await net.classify(webcam.capture());
+        const result = await net.predict(image);
         var end = new Date().getTime();
         var duration = end - start;
 
@@ -51,7 +33,8 @@ async function setup() {
     console.log('Loading mobilenet..');
 
     // Load the model.
-    net = await mobilenet.load();
+    //net = await mobilenet.load();
+    net = await tf.loadLayersModel('http://leonsthinkpad.lan:8000/MobileNetV2/model.json');
     console.log('Successfully loaded model');
 
     console.log("Used tf.js backend: " + tf.getBackend());
