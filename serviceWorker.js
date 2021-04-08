fdef697e21c66ecbd382c42ebefa4245bfa5646b
lonlazer <leon@lazar-rudolf.de>
2021-04-09 00:18:02 +0200

    Fix

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
 let cacheName = 'TFHoloPoC_Cache';
 let contentToCache = [
     'https://aframe.io/releases/1.2.0/aframe.min.js',
-    'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.7.4/dist/tf.min.js',
+    'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js',
     'https://cdn.aframe.io/fonts/mozillavr.fnt',
     'registerServiceWorker.js',
     'serviceWorker.js',
